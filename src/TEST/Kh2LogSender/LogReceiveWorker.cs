b0d3b7f93fd0f47b7895892cf3652338401c839a
조신연 <choshinyoun@naver.com>
2023-04-22 01:30:41 +0900

    second commit

--- src/TEST/Kh2LogSender/LogReceiveWorker.cs
+++ src/TEST/Kh2LogSender/LogReceiveWorker.cs
@@ -4,49 +4,48 @@ using Kafka.Public.Loggers;
 using System.Text;
 using System.Threading;
 
-namespace Kh2LogSender
+namespace Kh2LogSender;
+
+public class LogReceiveWorker : BackgroundService
 {
-    public class LogReceiveWorker : BackgroundService
+    private readonly ILogger<LogSendWorker> _logger;
+    private readonly ClusterClient _cluster;
+
+    public LogReceiveWorker(ILogger<LogSendWorker> logger)
     {
-        private readonly ILogger<LogSendWorker> _logger;
-        private readonly ClusterClient _cluster;
+        _logger = logger;
+        _cluster = new ClusterClient(new Configuration { 
+            Seeds = "localhost:9092"
+        }, new ConsoleLogger());
 
-        public LogReceiveWorker(ILogger<LogSendWorker> logger)
+    }
+
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cluster?.Dispose();
+        //return Task.CompletedTask;
+        return base.StopAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _cluster.ConsumeFromLatest(topic: "test");
+        _cluster.MessageReceived += record =>
         {
-            _logger = logger;
-            _cluster = new ClusterClient(new Configuration { 
-                Seeds = "localhost:9092"
-            }, new ConsoleLogger());
+            _logger.LogInformation($"Received:{Encoding.UTF8.GetString(record.Value as byte[])}");
+        };
 
-        }
-
-
-        public override Task StartAsync(CancellationToken cancellationToken)
-        {
-            return base.StartAsync(cancellationToken);
-        }
-
-        public override Task StopAsync(CancellationToken cancellationToken)
-        {
-            _cluster?.Dispose();
-            //return Task.CompletedTask;
-            return base.StopAsync(cancellationToken);
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            _cluster.ConsumeFromLatest(topic: "test");
-            _cluster.MessageReceived += record =>
-            {
-                _logger.LogInformation($"Received:{Encoding.UTF8.GetString(record.Value as byte[])}");
-            };
-
-            _logger.LogInformation("완료");
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
-            //    _logger.LogInformation("Worker2 running at: {time}", DateTimeOffset.Now);
-            //    await Task.Delay(1000, stoppingToken);
-            //}
-        }
+        _logger.LogInformation("완료");
+        //while (!stoppingToken.IsCancellationRequested)
+        //{
+        //    _logger.LogInformation("Worker2 running at: {time}", DateTimeOffset.Now);
+        //    await Task.Delay(1000, stoppingToken);
+        //}
     }
 }
--- src/TEST/Kh2LogSender/LogSendWorker.cs
+++ src/TEST/Kh2LogSender/LogSendWorker.cs
@@ -2,64 +2,64 @@ using Confluent.Kafka;
 using Smart.Kh2Ems.Infrastructure.WebServerApi;
 using System.Threading;
 
-namespace Kh2LogSender
+
+namespace Kh2LogSender;
+
+public class LogSendWorker : BackgroundService
 {
-    public class LogSendWorker : BackgroundService
+    private readonly ILogger<LogSendWorker> _logger;
+    private readonly WebApiContext _webContext;
+    private IProducer<Null, string> _producer;
+
+    public LogSendWorker(ILogger<LogSendWorker> logger, WebApiContext webContext)
     {
-        private readonly ILogger<LogSendWorker> _logger;
-        private readonly WebApiContext _webContext;
-        private IProducer<Null, string> _producer;
-
-        public LogSendWorker(ILogger<LogSendWorker> logger, WebApiContext webContext)
+        _logger = logger;
+        this._webContext = webContext;
+        var config = new ProducerConfig()
         {
-            _logger = logger;
-            this._webContext = webContext;
-            var config = new ProducerConfig()
+            BootstrapServers = "localhost:9092"
+        };
+        _producer = new ProducerBuilder<Null, string>(config).Build();
+
+    }
+
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _producer?.Dispose();
+        //return Task.CompletedTask;
+        return base.StopAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        int count = 0;
+        //for (var i = 0; i < 100; ++i)
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var value = await _webContext.GetComputerInfoToString();
+
+            // var value = $"Send Hello world {count++}";
+            _logger.LogInformation(value);
+            await _producer.ProduceAsync(topic: "test", new Message<Null, string>()
             {
-                BootstrapServers = "localhost:9092"
-            };
-            _producer = new ProducerBuilder<Null, string>(config).Build();
+                Value = value
+            }, stoppingToken);
 
+            await Task.Delay(1000, stoppingToken);
         }
 
+        _producer.Flush(timeout: TimeSpan.FromSeconds(10));
 
-        public override Task StartAsync(CancellationToken cancellationToken)
-        {
-            return base.StartAsync(cancellationToken);
-        }
-
-        public override Task StopAsync(CancellationToken cancellationToken)
-        {
-            _producer?.Dispose();
-            //return Task.CompletedTask;
-            return base.StopAsync(cancellationToken);
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            int count = 0;
-            //for (var i = 0; i < 100; ++i)
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                var value = await _webContext.GetComputerInfoToString();
-
-                // var value = $"Send Hello world {count++}";
-                _logger.LogInformation(value);
-                await _producer.ProduceAsync(topic: "test", new Message<Null, string>()
-                {
-                    Value = value
-                }, stoppingToken);
-
-                await Task.Delay(1000, stoppingToken);
-            }
-
-            _producer.Flush(timeout: TimeSpan.FromSeconds(10));
-
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
-            //    _logger.LogInformation("Worker1 running at: {time}", DateTimeOffset.Now);
-            //    await Task.Delay(1000, stoppingToken);
-            //}
-        }
+        //while (!stoppingToken.IsCancellationRequested)
+        //{
+        //    _logger.LogInformation("Worker1 running at: {time}", DateTimeOffset.Now);
+        //    await Task.Delay(1000, stoppingToken);
+        //}
     }
 }
